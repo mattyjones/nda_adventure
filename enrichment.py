be4ce656cc6f153343459d519e62ce77ab2b3837
Matt Jones <urlugal@gmail.com>
2017-04-11 09:14:20 -0400

    base class working

--- enrichment.py
+++ enrichment.py
@@ -14,43 +14,41 @@
 # common_actions = 'north, south, east, west, look, pickup, quit'
 # local_actions = 'no-op'
 
-class MyClass:
-    """A simple example class"""
-    i = 12345
+class Room:
+    description = ''
+    map = {}
 
-    def f(self):
-        return 'hello world'
+    def search(self):
+        print self.description
 
-matt_r1_map = {'south': 'r2', 'east': 'r3', 'west': 'r4'}
-matt_r2_map = {'north': 'r1', 'east': 'r5', 'west': 'r6'}
-matt_r1_description = """
+    def print_print(self):
+        print self.map
 
-Welcome to Matt's area. Be prepared for anything, you never know what is around the next corner or the next room.
 
-You are in a wide empty room with doors to the south, east, and west. There is a chair in the middle of the room and an empty table.
-"""
+# matt_r1_map = {'south': 'r2', 'east': 'r3', 'west': 'r4'}
+# matt_r2_map = {'north': 'r1', 'east': 'r5', 'west': 'r6'}
+# matt_r1_description = """
+#
+# Welcome to Matt's area. Be prepared for anything, you never know what is around the next corner or the next room.
+#
+# You are in a wide empty room with doors to the south, east, and west.
+# There is a chair in the middle of the room and an empty table.
+# """
 
-def help() :
-    if local_actions == '':
-      print('\ncommon actions: ' + common_actions + '\n')
-    else:
-      print('\ncommon actions: ' + common_actions + ', ' + local_actions + '\n')
-    
+
+def help():
     print('The simplest thing to do is look around\n')
 
-def look_around(room):
-    print room
 
 def move(direction, location):
-    # m = location + '_map'
-    print [direction]
+    print('this is a test')
 
 
 def leave_game():
     print('Good Bye!')
     exit(0)
 
-## -- Welcome the player and start the game -- ##
+# -- Welcome the player and start the game -- #
 print('Hello, welcome to the madhouse!\n')
 
 new_game = raw_input('Would you like to continue a previous game? (yes/no)\n')
@@ -58,12 +56,21 @@ if new_game == "yes":
     print('Sorry that feature is not implemented yet')
     exit(1)
 else:
-  print('Lets start a new game then\n')
-  loc = 'matt_r1'
-  print matt_r1_description # print the intro to your area
+    print('Lets start a new game then\n')
+    r1 = Room()
+    r1.description = """
 
-## -- Play the game -- ##
+Welcome to Matt's area. Be prepared for anything, you never know what is around the next corner or the next room.
 
+You are in a wide empty room with doors to the south, east, and west.
+There is a chair in the middle of the room and an empty table.
+"""
+    r1.map = {'south': 'r2', 'east': 'r3', 'west': 'r4'}
+
+  # loc = 'matt_r1'
+  print r1.description
+
+# -- Play the game -- #
 while action != 'quit':
 
     action = raw_input('What would you like to do? (help for list of available actions)\n')
@@ -83,6 +90,6 @@ while action != 'quit':
     elif action == 'quit':
         leave_game()
     else:
-      print(' Please enter a valid command\n')
-      help()    
+        print(' Please enter a valid command\n')
+        help()
 
